a324a480388b36ae91bdd175f25c4c6125291969
Jiaming Luo <j_luo@rosetta3.csail.mit.edu>
2018-11-24 15:20:45 -0500

    update data preparation scripts

--- data/ner/scripts/prepare_data.sh
+++ data/ner/scripts/prepare_data.sh
@@ -1,6 +1,6 @@
 #scp nayakne@jacob.stanford.edu:/u/nlp/data/ner/conll/eng.train ./train.txt
 #scp nayakne@jacob.stanford.edu:/u/nlp/data/ner/conll/eng.testa ./dev.txt
 
-cp /scratch2/jiang_guo/data/ner/conll03/train.txt ./train.txt
-cp /scratch2/jiang_guo/data/ner/conll03/valid.txt ./dev.txt
+#cp /scratch2/jiang_guo/data/ner/conll03/train.txt ./train.txt
+#cp /scratch2/jiang_guo/data/ner/conll03/valid.txt ./dev.txt
 python make_datasets.py ./ ../ 5
--- data/pos/scripts/prepare_data.sh
+++ data/pos/scripts/prepare_data.sh
@@ -1,8 +1,8 @@
 set -e
 #scp nayakne@jacob.stanford.edu:/u/nlp/data/pos-tagger/english/train-wsj-0-18 .
 #scp nayakne@jacob.stanford.edu:/u/nlp/data/pos-tagger/english/test-wsj-19-21 .
-cat /scratch2/jiang_guo/data/ptb/dependency-stanford-3.3.0/PTB_development_gold.conll | awk '{print $2"_"$4}' | python -c "import codecs; import sys; f = sys.stdin.read(); f = f.replace('\n_\n', '#####').replace('\n', ' ').replace('#####', '\n'); print f"  > train-wsj-0-18
-cat /scratch2/jiang_guo/data/ptb/dependency-stanford-3.3.0/PTB_train_gold.conll | awk '{print $2"_"$4}' | python -c "import codecs; import sys; f = sys.stdin.read(); f = f.replace('\n_\n', '#####').replace('\n', ' ').replace('#####', '\n'); print f"  > test-wsj-19-21
+#cat /scratch2/jiang_guo/data/ptb/dependency-stanford-3.3.0/PTB_development_gold.conll | awk '{print $2"_"$4}' | python -c "import codecs; import sys; f = sys.stdin.read(); f = f.replace('\n_\n', '#####').replace('\n', ' ').replace('#####', '\n'); print f"  > train-wsj-0-18
+#cat /scratch2/jiang_guo/data/ptb/dependency-stanford-3.3.0/PTB_train_gold.conll | awk '{print $2"_"$4}' | python -c "import codecs; import sys; f = sys.stdin.read(); f = f.replace('\n_\n', '#####').replace('\n', ' ').replace('#####', '\n'); print f"  > test-wsj-19-21
 
 curl -O https://raw.githubusercontent.com/slavpetrov/universal-pos-tags/master/en-ptb.map
 
@@ -14,7 +14,7 @@ echo "O" >> tagset.txt
 
 python make_datasets.py ./ ../  5
 
-rm train-wsj-0-18
-rm test-wsj-19-21 
+#rm train-wsj-0-18
+#rm test-wsj-19-21 
 rm train.txt
 rm dev.txt
